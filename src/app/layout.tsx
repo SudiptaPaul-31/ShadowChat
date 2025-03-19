69730885b2accb283cfe09c71accf8fabb5e5092
Ilya <ilaryndin30@gmail.com>
2025-03-19 14:55:23 +0200

    fix: changed title from Create Next App to ShadowChat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "ShadowChat",
-  description: "Privacy-focused,decentralized social network",
+  description: "Privacy-focused, decentralized social network",
 };
 
 export default function RootLayout({
